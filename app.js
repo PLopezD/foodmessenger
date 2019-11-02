f3cb30e58f05cdc31bf936d881b89c770ccbd1f8
Pablo Lopez Domowicz <pablolopezdomowicz@PablosPro.local>
2019-11-02 12:28:01 -0500

    forcing

--- app.js
+++ app.js
@@ -104,8 +104,8 @@ function handleMessage(sender_psid, received_message) {
       "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
     }
   }
-  
-  if (received_message.payload === 'SEEKER') {
+
+  if (received_message.quick_reply.payload === 'SEEKER') {
     response = startSeekerFlow()
   } 
   // Send the response message
