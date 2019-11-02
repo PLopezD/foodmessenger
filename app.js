f9c4c920a77ea9578d8966e7bc388268fea2ef95
Pablo Lopez Domowicz <pablolopezdomowicz@PablosPro.local>
2019-11-02 11:12:47 -0500

    testing

--- app.js
+++ app.js
@@ -57,7 +57,6 @@ app.post('/webhook', (req, res) => {
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
-        
         handlePostback(sender_psid, webhook_event.postback);
       }
       
@@ -102,9 +101,10 @@ app.get('/webhook', (req, res) => {
 
 function handleMessage(sender_psid, received_message) {
   let response;
-  
   // Checks if the message contains text
-  if (received_message.text) {    
+  console.log("received_message", received_message);
+  
+  if (received_message.text) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
