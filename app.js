335a30800d93ecac7d8416be022b7fc0a4344816
Pablo Lopez Domowicz <pablolopezdomowicz@PablosPro.local>
2019-11-02 15:42:37 -0500

    mvp progress

--- app.js
+++ app.js
@@ -107,9 +107,7 @@ function handleMessage(sender_psid, received_message) {
       "text": `Thankyou for providing your location ! Currently there are no food providers available ! `
       }
     } else {
-      response = {
-      "text": `Thankyou for providing your location ! The following food providers are near you : `
-      }
+      response = generateCarousel();
     }
     
     currentUser['stepType'] = 'finish';
@@ -240,4 +238,38 @@ function generateQuickReplies() {
     }
   ]
   return genQuickReply("What are you?", quickReplies)
+}
+
+function generateCarousel() {
+  return {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"Welcome!",
+            "image_url":"https://petersfancybrownhats.com/company_image.png",
+            "subtitle":"We have the right hat for everyone.",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://petersfancybrownhats.com/view?item=103",
+              "webview_height_ratio": "tall",
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersfancybrownhats.com",
+                "title":"View Website"
+              },{
+                "type":"postback",
+                "title":"Start Chatting",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              }              
+            ]      
+          }
+        ]
+      }
+    }
+  }
 }
